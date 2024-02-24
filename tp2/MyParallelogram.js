105f94b476c91407152a65ed4754912065f675b6
Bartek Pacia <up202311689@fe.up.pt>
2024-02-24 15:10:04 +0100

    copy files from tp1 into tp2

---
+++ tp2/MyParallelogram.js
@@ -0,0 +1,34 @@
+import {CGFobject} from '../lib/CGF.js';
+
+export class MyParallelogram extends CGFobject {
+	constructor(scene) {
+		super(scene);
+		this.initBuffers();
+	}
+
+	initBuffers() {
+		this.vertices = [
+			 0,  0,  0,	//0
+			 2,  0,  0,	//1
+			 3,  1,  0,	//2
+			 1,  1,  0,	//3
+		];
+
+		//Counter-clockwise reference of vertices
+		this.indices = [
+			// frontview (CCW winding order)
+			3, 0, 1,
+			2, 3, 1,
+			// backview (CW winding order)
+			1, 0, 3,
+			1, 3, 2,
+		];
+
+		//The defined indices (and corresponding vertices)
+		//will be read in groups of three to draw triangles
+		this.primitiveType = this.scene.gl.TRIANGLES;
+
+		this.initGLBuffers();
+	}
+}
+
