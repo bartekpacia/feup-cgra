898b77a8359e2bfd1d2f7aac824c80dfa20c0f46
Bartek Pacia <up202311689@fe.up.pt>
2024-03-06 18:45:45 +0000

    TP3: vertices + indices idea

--- tp3/MyPrism.js
+++ tp3/MyPrism.js
@@ -15,26 +15,42 @@ export class MyPrism extends CGFobject {
         // draw prism bottom
         let alpha = 0;
         const step = 2 * Math.PI / this.slices;
-        for (let i = 0; i < this.slices; i++) {
-            const vertex = [Math.cos(alpha), Math.sin(alpha), 0];
-			console.log(`vertex ${i}: ${vertex}`)
-            this.vertices.push(...vertex);
-            alpha += step;
+		let currentStackHeight = 0;
+
+		const stackHeightDelta = 1 / this.stacks;
+		while (currentStackHeight < 1) {
+			for (let i = 0; i < this.slices; i++) {
+				const vertex0 = [Math.cos(i * step), Math.sin(i * step), currentStackHeight];
+				const vertex1 = [Math.cos((i * step) + 1), Math.sin((i * step) + 1), currentStackHeight];
+				const vertex2 = [Math.cos((i * step) + 1), Math.sin((i * step) + 1), currentStackHeight + stackHeightDelta];
+				const vertex3 = [Math.cos(i * step), Math.sin(i * step), currentStackHeight + stackHeightDelta];
+
+				this.vertices.push(...vertex0, ...vertex3, ...vertex1, ...vertex2);
+
+				//console.log(`vertex ${i}: ${vertex}`)
+				//this.vertices.push(...vertex);
+				alpha += step;
+			}
+			currentStackHeight += stackHeightDelta;
+		}
+
+		let index = 0;
+        for (let i = 0; i < this.stacks; i++){
+            for (let j = 0; j < this.slices; j++){
+                this.indices.push(index,j+2,j+3);
+                this.indices.push(index,j+3,j+1);
+                index += 4; 
+            }
+           
         }
 
-		this.indices = [
-			0, 1, 2,
-			0, 2, 3,
-			0, 3, 4,
-		];
-
-		this.normals = [
-			0, 0, 1,
-			0, 0, 1,
-			0, 0, 1,
-			0, 0, 1,
-			0, 0, 1,
-		];
+		// this.normals = [
+		// 	0, 0, 1,
+		// 	0, 0, 1,
+		// 	0, 0, 1,
+		// 	0, 0, 1,
+		// 	0, 0, 1,
+		// ];
 
 		this.primitiveType = this.scene.gl.TRIANGLES;
 		this.initGLBuffers();
