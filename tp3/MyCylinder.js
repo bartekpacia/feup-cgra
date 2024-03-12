28ee2ee35c60a8311b655ba1893a8a247518be26
Bartek Pacia <up202311689@fe.up.pt>
2024-03-12 18:08:45 +0000

    MyCylinder: fix weird off by one error

--- tp3/MyCylinder.js
+++ tp3/MyCylinder.js
@@ -24,26 +24,25 @@ export class MyCylinder extends CGFobject {
 
 			currentStackHeight += stackHeightDelta;
 		}
-
+		
 		for (let i = 0; i < this.stacks; i++) {
             // Offset from indices of vertices in the first stack
 			const offset = this.slices * i;
 			for (let j = 0; j < this.slices; j++) {
                 const triangle0 = [offset + j,     offset + j+1, offset + j + this.slices];
 				this.indices.push(...triangle0);
-                
-                const triangle1 = [offset + j + 1, offset + j + this.slices + 1, offset + j + this.slices];
+                let triangle1 = [offset + j + 1, offset + j + this.slices + 1, offset + j + this.slices];
                 console.log(`face ${i * offset + j}: ${triangle0} and ${triangle1}`);
-                if (i == this.stacks - 1 && j == this.slices - 1) {
-                    continue
+
+                // Prevent off-by-one error
+                if ((j + 1) % this.slices == 0) {
+                    triangle1[0] -= this.slices;
+                    triangle1[1] -= this.slices;
+                    triangle1[2] -= this.slices; 
                 }
-				this.indices.push(...triangle1);
+                this.indices.push(...triangle1);
 			}
 		}
-
-        // this.indices.push(1, 7, 6);
-        // this.indices.push(2, 8, 7);
-        // this.indices.push(8, 13, 12);
 
 		this.primitiveType = this.scene.gl.TRIANGLES;
 		this.initGLBuffers();
